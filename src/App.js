74506966a979305df8eb65e5513210c26802b7b2
Melissa <lynch.melissat@gmail.com>
2022-10-19 18:26:53 -0400

    adding clock component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Melissa Lynch';
+  const element1 = <h1>Hello, {name}</h1>;
+
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+  
+  const element2 = (
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {element1}
     </div>
   );
 }
